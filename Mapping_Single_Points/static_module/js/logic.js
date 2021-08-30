8d44a3dd03d97776647294d76ccede5e3ba49679
danielbrock4 <daniel.brock4@outlook.com>
2021-08-30 16:59:09 -0500

    added multiple makers through circles.js array and changed the colors

--- Mapping_Single_Points/static_module/js/logic.js
+++ Mapping_Single_Points/static_module/js/logic.js
@@ -20,7 +20,7 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 //         40.7, -94.5
 //     ],
 //     zoom: 4
-// });    
+// }); 
 
 // ADD TILE LAYER FOR OUR MAP
 
@@ -71,3 +71,115 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tile
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
 /* */
+
+// ADD A MARKER TO MAP - Found in provided code https://leafletjs.com/examples/quick-start/
+// Provided Code
+// var marker = L.marker([51.5, -0.09]).addTo(map);
+//  Add a marker to the map for Los Angeles, California.
+// var marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+
+// ADD A CIRCLE TO MAP - Found in provided code https://leafletjs.com/examples/quick-start/
+let circle = L.circle([34.0522, -118.2437], {
+    radius: 300, // measured in meters so may need to be bigger
+    color: 'black',
+    fillColor: '#ffff00',
+    fillOpacity: 0.25,
+}).addTo(map);
+// Alternatively, we can create a circle using the circleMarker() function. 
+// The circleMarker() function measures the radius of the circle in pixels, with the default radius set at 10 pixels
+
+// ADD MULTIPLE MARKERS
+    // To updated our Markers function we need an array with multiple markers
+// An array containing each city's location, state, and population.
+let citiesExample = [{
+    location: [40.7128, -74.0059],
+    city: "New York City",
+    state: "NY",
+    population: 8398748
+  },
+  {
+    location: [41.8781, -87.6298],
+    city: "Chicago",
+    state: "IL",
+    population: 2705994
+  },
+  {
+    location: [29.7604, -95.3698],
+    city: "Houston",
+    state: "TX",
+    population: 2325502
+  },
+  {
+    location: [34.0522, -118.2437],
+    city: "Los Angeles",
+    state: "CA",
+    population: 3990456
+  },
+  {
+    location: [33.4484, -112.0740],
+    city: "Phoenix",
+    state: "AZ",
+    population: 1660272
+  }
+  ];
+// We need to iterate through each city object and add each city location to the marker() function, which will, 
+// in turn, be added to the map. Test with Console.Log first
+// For Loop Formulas
+     // for (let i = 0; i < cities.length; i++)
+    // cities.forEach(function(city))
+
+// Loop through the cities array and create one marker for each city.
+// citiesExample.forEach((citiesObj) => {
+//     console.log(citiesObj.city);
+//      L.marker(citiesObj.location).addTo(map)
+// }); 
+
+/*When handling large datasets, it's a best practice to have the data in an external file and refer 
+to that file and dataset in the logic.js file. Next, in the logic.js file, where the cities array was located, 
+add a variable and assign it to the cities array. */
+
+// Get data from cities.js
+let cityData = cities
+
+// Loop through the cities array and create one marker for each city.
+// cityData.forEach((citiesObj) => {
+//     console.log(citiesObj.city);
+//     L.marker(citiesObj.location).addTo(map)
+// }); 
+
+// BIND POP TO THE MAKER
+/* To add data from each object in the cities array, we'll use Leaflet's bindPopup() method on the marker() function.
+ According to the guidance in the Quick Start Guide's "Working with popups" section, we only need to add 
+ HTML code inside the parentheses of the bindPopup() method. Edit the forEach function and add the bindPopup() method. 
+Inside the parentheses of the bindPopup() method, we'll retrieve the name of the city, state, and population.*/ 
+
+// Loop through the cities array and create one marker for each city.
+cityData.forEach((citiesObj) => {
+    console.log(citiesObj.city);
+    L.circleMarker(citiesObj.location, {
+        // Add radii by using population & decrease each city's radius so the circle markers fit on the map
+        radius: citiesObj.population/200000,
+        color: '#ffa500', // line color
+        weight: 4, // lineweight
+        fillColor: '#ffa500', // filled circle color
+        fillOpacity: 0.25,
+    })
+    // Add bindPopup() on L.Marker() and retrieve the name of the city, state, and population
+        // Add toLocaleString() on the city.population to format the population with a thousands separator
+        .bindPopup("<h2>" + citiesObj.city + ", " + citiesObj.state + 
+                "</h2> <hr> <h3>Population " + citiesObj.population.toLocaleString() + "</h3>" )
+    // Add addTo(map) on L.Marker() to add the graymap object tile layer to our let map
+        .addTo(map); 
+}); 
+
+  /* */
+  /* 1)
+  2)
+  3)
+  4)
+  5)
+  6)
+  7)
+  8)
+  9) */
